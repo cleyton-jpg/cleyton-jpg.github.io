ec4db1802b6105eb2828b7291989ece93e3b0446
cleyton-jpg <cleytonjulio88@gmail.com>
2022-12-09 11:31:36 -0300

    everything finally working as intended

--- projetos/campoMinado/gameLogic.js
+++ projetos/campoMinado/gameLogic.js
@@ -1,38 +1,33 @@
-let size = 10;
-let bomb = 15;
-let camp = new Array(size);
+var size = 10;
+var bomb = 40;
+var camp = new Array(size).fill(0).map(() => new Array(size).fill(0));
 
-for (let i = 0; i < camp.length; i++) {
-    camp[i] = new Array(size);
-    for (let j = 0; j < camp.length; j++) {
-        camp[i][j] = 0;
+function updateNeighbors() {
+  for (var h = -1; h < 2; h++) {
+    for (var l = -1; l < 2; l++) {
+      check =
+        Y + l >= 0 &&
+        Y + l < size &&
+        X + h >= 0 &&
+        X + h < size &&
+        camp[Y + l][X + h] != "B";
+      if (check) {
+        camp[Y + l][X + h]++;
+      }
     }
+  }
 }
 
-function cordenates(param) {
-    return Math.floor(Math.random() * (param));
+for (let index = 0; index < bomb; ) {
+  var Y = Math.floor(Math.random() * size);
+  var X = Math.floor(Math.random() * size);
+  if (camp[Y][X] !== "B") {
+    camp[Y][X] = "B";
+    index++;
+    updateNeighbors();
+  }
 }
 
-for (let index = 0; index < bomb;) {
-    var Y = cordenates(camp.length);
-    var X = cordenates(camp.length);
-    if (camp[Y][X] != 'B') {
-        camp[Y][X] = 'B';
-        var area = [Y +1, X -1, Y +1, X, Y +1, X +1,
-                    Y, X -1, Y, X +1,
-                    Y -1, X -1, Y -1, X, Y -1, X +1]
-        
-        for (let index = 0; index < area.length;) {
-            if ((area[index] && area[index + 1] < size) &&
-                (area[index] && area[index + 1] >= 0)) {
-                if(camp[area[index]][area[index + 1]] != 'B')
-                {camp[area[index]][area[index + 1]]++}
-            }
-            index +=2
-        }
-        index++
-    }
-}
-for (let index = 0; index < camp.length; index++) {
-    console.log(camp[index].toString());
-}
+camp.forEach((element) => {
+  console.log(element.toString());
+});
