665766465d489f57d302429a662169eafee62bb7
cleyton-jpg <cleytonjulio88@gmail.com>
2022-12-14 21:50:59 -0300

    final touchups to the game logic and html staging

--- projetos/campoMinado/gameLogic.js
+++ projetos/campoMinado/gameLogic.js
@@ -1,33 +1,31 @@
-var size = 10;
-var bomb = 40;
-var camp = new Array(size).fill(0).map(() => new Array(size).fill(0));
+function main(size, bomb) {
+  let camp = new Array(size).fill(0).map(() => new Array(size).fill(0));
 
-function updateNeighbors() {
-  for (var h = -1; h < 2; h++) {
-    for (var l = -1; l < 2; l++) {
-      check =
-        Y + l >= 0 &&
-        Y + l < size &&
-        X + h >= 0 &&
-        X + h < size &&
-        camp[Y + l][X + h] != "B";
-      if (check) {
-        camp[Y + l][X + h]++;
+  for (let index = 0; index < bomb;) {
+    var Y = Math.floor(Math.random() * size);
+    var X = Math.floor(Math.random() * size);
+    if (camp[Y][X] !== "B") {
+      camp[Y][X] = "B";
+      index++;
+      updateNeighbors();
+    }
+  }
+
+  function updateNeighbors() {
+    for (let h = -1; h < 2; h++) {
+      for (let l = -1; l < 2; l++) {
+        check =
+          Y + l >= 0 && Y + l < size &&
+          X + h >= 0 && X + h < size &&
+          camp[Y + l][X + h] != "B";
+        if (check) {
+          camp[Y + l][X + h]++;
+        }
       }
     }
   }
+  camp.forEach((element) => {
+    console.log(element.toString());
+  });  
 }
-
-for (let index = 0; index < bomb; ) {
-  var Y = Math.floor(Math.random() * size);
-  var X = Math.floor(Math.random() * size);
-  if (camp[Y][X] !== "B") {
-    camp[Y][X] = "B";
-    index++;
-    updateNeighbors();
-  }
-}
-
-camp.forEach((element) => {
-  console.log(element.toString());
-});
+main(10, 50)
