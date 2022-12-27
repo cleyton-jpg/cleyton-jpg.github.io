931784d0e51c5021fc7f928e4d905b05f6fb5261
cleyton-jpg <cleytonjulio88@gmail.com>
2022-12-27 08:04:14 -0300

    the commits will continue untill program improves

--- gameLogic.js
+++ gameLogic.js
@@ -29,3 +29,23 @@ function logic(size, bomb) {
 }
 logic(parseInt(size.value),parseInt(bomb.value));
 console.log(size.value, bomb.value);
+function createDiv(className, id) {
+  // create a new div element
+  var div = document.createElement("div");
+  
+  // add the specified class to the div
+  div.classList.add(className);
+
+  div.id = id;
+  
+  // add some content to the div
+  div.innerHTML = "Hello World!";
+  
+  // add the div to the document
+  document.body.appendChild(div);
+}
+for (let index = 0; index < camp; index++) {
+  const element = array[index];
+  
+}
+createDiv(card, )
